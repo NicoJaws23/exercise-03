815bf1e5c2989d76dd4f8120e8029d163ecac9be
NicoJaws23 <nico.jaworski@utexas.edu>
2025-02-03 09:15:45 -0600

    sixth commit

--- EDA_Challenge_Code.R
+++ EDA_Challenge_Code.R
@@ -5,47 +5,54 @@ d <- read.csv(f, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 head(d)
 
 #Set Variables
-BSD <- d$Body_mass_male_mean/d$Body_mass_female_mean
-head(BSD)
-sex_ratio <- d$AdultFemale/d$AdultMales
-head(sex_ratio)
-DI <- d$DayLength_km/(pi * d$HomeRange_km2)
-head(DI)
+library(dplyr)
+library(tidyverse)
+d <- d %>%
+  mutate(d, BSD = Body_mass_male_mean/Body_mass_female_mean) %>%
+  mutate(d, sex_ratio = AdultFemale/AdultMales) %>%
+  mutate(d, DI = DayLength_km/(2 * sqrt(HomeRange_km2/pi)))
+names(d)
+#BSD <- d$Body_mass_male_mean/d$Body_mass_female_mean
+#head(BSD)
+#sex_ratio <- d$AdultFemale/d$AdultMales
+#head(sex_ratio)
+#DI <- d$DayLength_km/(pi * d$HomeRange_km2)
+#head(DI)
 
 #plot the data
 library(ggplot2)
 library(dplyr)
 #Movement and day length overall
 ggplot(data = d, mapping = aes(x = Move, y = DayLength_km)) +
-  geom_point() +
+  geom_point(na.rm = TRUE) +
   ggtitle("Movement and Day Range Length Overall")
 
 #Movement and day length by family
 ggplot(data = d, mapping = aes(x = Move, y = DayLength_km)) +
-  geom_point() +
+  geom_point(na.rm = TRUE) +
   ggtitle("Movement and Day Range Length by Family") +
   facet_wrap(~Family)
 
 #Group size and day range length overall
 ggplot(data = d, mapping = aes(x = MeanGroupSize, y = DayLength_km)) +
-  geom_point() +
+  geom_point(na.rm = TRUE) +
   ggtitle("Group Size and Day Range Length Overall")
 
 #Group size and day range by family
 ggplot(data = d, mapping = aes(x = MeanGroupSize, y = DayLength_km)) +
-  geom_point() +
+  geom_point(na.rm = TRUE) +
   ggtitle("Group Size and Day Range Length by Family") +
   facet_wrap(~Family)
 
 
 #Canine Dimorphism and Body Dimorphism Overall
 ggplot(data = d, mapping = aes(x = BSD, y = Canine_Dimorphism)) +
-  geom_point() +
+  geom_point(na.rm = TRUE) +
   ggtitle("Body Size and Canine Size Dimorphism Overall")
 
 #Canine Dimorphism and Body Dimorphism by Familt
 ggplot(data = d, mapping = aes(x = BSD, y = Canine_Dimorphism)) +
-  geom_point() +
+  geom_point(na.rm = TRUE) +
   ggtitle("Body Size and Canine Size Dimorphism by Family") +
   facet_wrap(~Family)
 
@@ -59,7 +66,7 @@ d <- d %>%
 
 #now we are going to use box plots to plot 
 ggplot(data = d, mapping = aes(x = diet_strategy, y = MeanGroupSize)) +
-  geom_boxplot() +
+  geom_boxplot(na.rm = TRUE) +
   ggtitle("Diet Strategy and Mean Group Size")
 
 #Here I will use tools from the {dplyr} package to create new variables in the data
@@ -71,4 +78,4 @@ d <- d %>%
   group_by(Family) %>%
   summarize(avgBrainSize = mean(Brain_Size_Species_Mean, na.rm = TRUE), avgBodyMassMale = mean(Body_mass_male_mean, na.rm = TRUE)) %>%
   arrange(avgBrainSize)
-d
+print(d)
